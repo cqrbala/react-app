4c57dd9c26f3a8df3643c925d5e2508e2ad3a0e2
cqrbala <vbalakumar2003@gmail.com>
2023-01-24 02:38:39 +0530

    Updates

---
+++ precache-manifest.25d20112f04b1163c8d6341d36da3b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3421fdfe38780c2b4b34",
+    "url": "/react-app/static/css/main.89da8a45.chunk.css"
+  },
+  {
+    "revision": "3421fdfe38780c2b4b34",
+    "url": "/react-app/static/js/main.3421fdfe.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/react-app/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "c19aa01c5f9fe896939b",
+    "url": "/react-app/static/js/2.c19aa01c.chunk.js"
+  },
+  {
+    "revision": "c98911c019955331f22d",
+    "url": "/react-app/static/js/runtime~main.c98911c0.js"
+  },
+  {
+    "revision": "64dd5412bea04451d29514ab6d6381a8",
+    "url": "/react-app/index.html"
+  }
+];
